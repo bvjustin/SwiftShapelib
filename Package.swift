ee5fa4999512bbbbe4f4fb7838e0eabdcdbde2e2
Justin Greenfield <justin.greenfield@dtn.com>
2020-04-08 11:10:59 -0500

    update

--- Package.swift
+++ Package.swift
@@ -4,9 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftShapelib",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ]
+    name: "CShapelib",
+    providers: [
+	.brew(["shapelib"])
+		]
 )
---
+++ shim.h
@@ -0,0 +1,6 @@
+#ifndef CLIB_SWIFT_SHAPELIB
+#define CLIB_SWIFT_SHAPELIB
+
+#import <shapefil.h>;
+
+#endif
